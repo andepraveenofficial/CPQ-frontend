23275b560b8804c986b4ddef3b4a643e7d208158
Ande Praveen <praveenande2023@gmail.com>
2024-07-19 10:11:29 +0530

    implemented Production Page

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, Spin, Button } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
-const Products: React.FC = () => {
-  return <div>Products</div>;
+interface IProduct {
+  id: number;
+  name: string;
+  internal_name: string;
+  description: string;
+  charge_method: string;
+  currency: string;
+  unit_price: number;
+  status: string;
+  last_activity: string;
+}
+
+const ProductsTable: React.FC = () => {
+  const [data, setData] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const columns: ColumnsType<IProduct> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Pricing',
+      dataIndex: 'charge_method',
+      key: 'charge_method',
+    },
+    {
+      title: 'Currency',
+      dataIndex: 'currency',
+      key: 'currency',
+    },
+
+    {
+      title: 'Last Activity',
+      dataIndex: 'last_activity',
+      key: 'last_activity',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      render: (text, record) => {
+        return (
+          <Button
+            type="dashed"
+            onClick={() => {
+              // return handleStatusClick();
+              console.log(record);
+            }}
+          >
+            {text}
+          </Button>
+        );
+      },
+    },
+  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = 'http://localhost:5000/api/v1/products';
+        const jwtToken = Cookies.get('jwtToken');
+
+        const response = await axios.get<IProduct[]>(url, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${jwtToken}`,
+          },
+        });
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Spin />;
+  }
+
+  return <Table dataSource={data} columns={columns} rowKey="id" />;
 };
 
-export default Products;
+export default ProductsTable;
